6f1339a5526fe637eb1e0070de7f360bbc03969a
LaDerrius Williams <mrldaniellewilliams@gmail.com>
2016-09-04 18:43:32 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop(array) {
+  for (let i = 1; i < array.length; i++) {
+    if (i === 1 && i != 0) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+}
